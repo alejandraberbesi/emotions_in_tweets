3742d2ad8c186dbfcfcc69c5515e00d60349b83c
Alejandra_Berbesi <berbesi.alejandra@gmail.com>
2019-10-30 20:49:48 -0500

    initial model

---
+++ src/emotion_classification.py
@@ -0,0 +1,34 @@
+import re
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+from nltk.corpus import stopwords
+from sklearn.feature_extraction.text import TfidfVectorizer
+from nltk.stem import PorterStemmer
+
+df = pd.read_csv('data/train_data.csv')
+
+a=df.loc[0,'content']
+
+# cleaning steps:
+def filter(text):
+    text2 = text.split()
+    text2 = [word for word in text2 if word.isalpha()]
+    text2 = ' '.join(text2)
+    return text2
+
+for index, row in df.iterrows():
+    df.loc[index,'content']  = filter(df.loc[index,'content'])
+
+tfidfconverter = TfidfVectorizer(max_features=1500,stop_words=stopwords.words('english'))
+X = tfidfconverter.fit_transform(df['content']).toarray()
+y=np.array(df['sentiment'])
+
+X_train, X_test, y_train, y_test = train_test_split(X, y,test_size=0.20,random_state = 10)
+clf = RandomForestClassifier(n_estimators=500, random_state=100, class_weight = "balanced")
+clf.fit(X_train, y_train)
+y_pred=clf.predict(X_test)
+
+print(accuracy_score(y_test, y_pred))
