8a09e2d0b65a6f56c30a8dc79807b0616cafee38
Alejandra_Berbesi <berbesi.alejandra@gmail.com>
2019-10-30 20:53:10 -0500

    model with stemming

--- src/emotion_classification.py
+++ src/emotion_classification.py
@@ -11,11 +11,11 @@ from nltk.stem import PorterStemmer
 df = pd.read_csv('data/train_data.csv')
 
 a=df.loc[0,'content']
-
+stemming=PorterStemmer()
 # cleaning steps:
 def filter(text):
     text2 = text.split()
-    text2 = [word for word in text2 if word.isalpha()]
+    text2 = [stemming.stem(word) for word in text2 if word.isalpha()]
     text2 = ' '.join(text2)
     return text2
 
@@ -27,7 +27,7 @@ X = tfidfconverter.fit_transform(df['content']).toarray()
 y=np.array(df['sentiment'])
 
 X_train, X_test, y_train, y_test = train_test_split(X, y,test_size=0.20,random_state = 10)
-clf = RandomForestClassifier(n_estimators=500, random_state=100, class_weight = "balanced")
+clf = RandomForestClassifier(n_estimators=500, random_state=100, class_weight = "ball")
 clf.fit(X_train, y_train)
 y_pred=clf.predict(X_test)
 
