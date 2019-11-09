2c5322b5b602ba9ac27c6910c625728bb1ced92e
Alejandra_Berbesi <berbesi.alejandra@gmail.com>
2019-11-09 17:34:58 -0500

    model with rnn

--- src/emotion_classification.py
+++ src/emotion_classification.py
@@ -1,34 +1,63 @@
 import re
 import pandas as pd
-import numpy as np
-from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import accuracy_score
+import tensorflow as tf
+import os
 from nltk.corpus import stopwords
-from sklearn.feature_extraction.text import TfidfVectorizer
-from nltk.stem import PorterStemmer
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from sklearn.model_selection import train_test_split
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Embedding, LSTM, SpatialDropout1D
+from tensorflow.keras.callbacks import EarlyStopping
 
-df = pd.read_csv('data/train_data.csv')
-stemming=PorterStemmer()
-print(df.groupby('sentiment').count()) #categories are unbalanced
+#with Long short-term memory (LSTM)-RNN
 
-# cleaning steps:
-def filter(text):
-    text2 = text.split()
-    text2 = [stemming.stem(word) for word in text2 if word.isalpha()]
-    text2 = ' '.join(text2)
-    return text2
+df=pd.read_csv('data/train_data.csv')
+#df.sentiment.value_counts()
 
-for index, row in df.iterrows():
-    df.loc[index,'content']  = filter(df.loc[index,'content'])
+REPLACE_BY_SPACE_RE = re.compile('[/(){}\[\]\|@,;]')
+STOPWORDS = set(stopwords.words('english'))
 
-tfidfconverter = TfidfVectorizer(max_features=1500,stop_words=stopwords.words('english'))
-X = tfidfconverter.fit_transform(df['content']).toarray()
-y=np.array(df['sentiment'])
+def clean_text(text):
+    text = text.lower() # lowercase text
+    text = REPLACE_BY_SPACE_RE.sub(' ', text) 
+    text = ' '.join(word for word in text.split() if word not in STOPWORDS) # remove stopwors from text
+    return text
 
-X_train, X_test, y_train, y_test = train_test_split(X, y,test_size=0.20,random_state = 10)
-clf = RandomForestClassifier(n_estimators=100, random_state=100,class_weight = "balanced")
-clf.fit(X_train, y_train)
-y_pred=clf.predict(X_test)
+df['content'] = df['content'].apply(clean_text)
+df['content'] = df['content'].str.replace('\d+', '') #remove digits
 
-print(accuracy_score(y_test, y_pred))
+# The maximum number of words to be used
+MAX_NB_WORDS = 50000
+# Max number of words in each tweet.
+MAX_SEQUENCE_LENGTH = 250
+EMBEDDING_DIM = 100
+
+tokenizer = Tokenizer(num_words=MAX_NB_WORDS, filters='!"#$%&()*+,-./:;<=>?@[\]^_`{|}~', lower=True)
+tokenizer.fit_on_texts(df['content'].values)
+word_index = tokenizer.word_index
+#print('Found %s unique tokens.' % len(word_index))
+
+X = tokenizer.texts_to_sequences(df['content'].values)
+X = pad_sequences(X, maxlen=MAX_SEQUENCE_LENGTH)
+#print('Shape of data tensor:', X.shape)
+
+Y = pd.get_dummies(df['sentiment']).values
+#print('Shape of label tensor:', Y.shape)
+
+X_train, X_test, Y_train, Y_test = train_test_split(X,Y, test_size = 0.20, random_state = 100)
+
+model = Sequential()
+model.add(Embedding(MAX_NB_WORDS, EMBEDDING_DIM, input_length=X.shape[1]))
+model.add(SpatialDropout1D(0.2))
+model.add(LSTM(100, dropout=0.2, recurrent_dropout=0.2))
+model.add(Dense(13, activation='softmax'))
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+epochs = 5
+batch_size = 64
+
+history = model.fit(X_train, Y_train, epochs=epochs, batch_size=batch_size,validation_split=0.1,callbacks=[EarlyStopping(monitor='val_loss', patience=3, min_delta=0.0001)])
+
+accr = model.evaluate(X_test,Y_test)
+print('Test set\n  Loss: {:0.3f}\n  Accuracy: {:0.3f}'.format(accr[0],accr[1]))
