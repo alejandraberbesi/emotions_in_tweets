2245e61b9015cf43d51b88663fd2a2a66f7eb9c9
Alejandra_Berbesi <berbesi.alejandra@gmail.com>
2019-12-10 16:58:47 -0500

    epochs graphs

--- src/emotion_classification.py
+++ src/emotion_classification.py
@@ -9,6 +9,7 @@ from sklearn.model_selection import train_test_split
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Embedding, LSTM, SpatialDropout1D
 from tensorflow.keras.callbacks import EarlyStopping
+import matplotlib.pylab as plt
 
 #with Long short-term memory (LSTM)-RNN
 
@@ -65,3 +66,25 @@ history = model.fit(X_train, Y_train, epochs=epochs, batch_size=batch_size,valid
 
 accr = model.evaluate(X_test,Y_test)
 print('Test set\n  Loss: {:0.3f}\n  Accuracy: {:0.3f}'.format(accr[0],accr[1]))
+
+acc = history.history['accuracy']
+val_acc = history.history['val_accuracy']
+
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+epochs_range = range(epochs)
+
+plt.figure(figsize=(8, 5))
+plt.subplot(1, 2, 1)
+plt.plot(epochs_range, acc, label='Training Accuracy')
+plt.plot(epochs_range, val_acc, label='Validation Accuracy')
+plt.legend(loc='lower right')
+plt.title('Training and Validation Accuracy')
+
+plt.subplot(1, 2, 2)
+plt.plot(epochs_range, loss, label='Training Loss')
+plt.plot(epochs_range, val_loss, label='Validation Loss')
+plt.legend(loc='upper right')
+plt.title('Training and Validation Loss')
+plt.show()
