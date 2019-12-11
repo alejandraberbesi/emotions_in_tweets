0fbc6739a24e3e6dc655798d9b8655d6e0a58e3e
Alejandra_Berbesi <berbesi.alejandra@gmail.com>
2019-12-11 15:08:07 -0500

    code changes

--- src/emotion_classification.py
+++ src/emotion_classification.py
@@ -11,46 +11,38 @@ from tensorflow.keras.layers import Dense, Embedding, LSTM, SpatialDropout1D
 from tensorflow.keras.callbacks import EarlyStopping
 import matplotlib.pylab as plt
 
-#with Long short-term memory (LSTM)-RNN
-
 df=pd.read_csv('data/train_data.csv')
-#df.sentiment.value_counts()
+#df.sentiment.value_counts() #unbalanced data, 13 categories
 
-REPLACE_BY_SPACE_RE = re.compile('[/(){}\[\]\|@,;]')
 STOPWORDS = set(stopwords.words('english'))
-
 def clean_text(text):
-    text = text.lower() # lowercase text
-    text = REPLACE_BY_SPACE_RE.sub(' ', text) 
-    text = ' '.join(word for word in text.split() if word not in STOPWORDS) # remove stopwors from text
+    text = text.lower()
+    text = ' '.join(word for word in text.split() if word not in STOPWORDS) 
     return text
 
 df['content'] = df['content'].apply(clean_text)
 df['content'] = df['content'].str.replace('\d+', '') #remove digits
 
-
-MAX_NB_WORDS = 50000
-MAX_SEQUENCE_LENGTH = 250
-EMBEDDING_DIM = 100
-
-#to vectorize a text corpus
-#num_words: the maximum number of words to keep, based on word frequency. 
-#filters: more characters to exclude
-tokenizer = Tokenizer(num_words=MAX_NB_WORDS, filters='=!"#$%&*+-.:<=>?\^_`~')
-#This method creates the vocabulary index based on word frequency,lower integer means more frequent word
+#convert each word into an index
+tokenizer = Tokenizer(filters='\n\t!"#$%&()*+,-./:;<=>?[\]^_`{|}~ ')
 tokenizer.fit_on_texts(df['content'].values)
 word_index = tokenizer.word_index 
-#print('%s unique tokens' % len(word_index))
 
-#Transform each text in a sequence of integers
-X = tokenizer.texts_to_sequences(df['content'].values)
-#to produce batches for training/validation
-X = pad_sequences(X, maxlen=MAX_SEQUENCE_LENGTH)
-#print('Shape of data tensor:', X.shape)
-Y = pd.get_dummies(df['sentiment']).values #from categorical to dummy
-#print('Shape of label tensor:', Y.shape)
+#Transform each tweet in a sequence of integers/indexes of words
+a = tokenizer.texts_to_sequences(df['content'].values)
+b=list()
+for i in a:
+    b.append(len(i)) 
+MAX_SEQUENCE_LENGTH = max(b) #largest length of filtered tweets
+#arrays with zeros and/or words indexes:
+X = pad_sequences(a, maxlen=MAX_SEQUENCE_LENGTH)
+#emotions categories classified with arrays of zeros and ones
+Y = pd.get_dummies(df['sentiment']).values
 
+#test data= 20% of total data
 X_train, X_test, Y_train, Y_test = train_test_split(X,Y, test_size = 0.20, random_state = 100)
+
+EMBEDDING_DIM = 100
 
 model = Sequential()
 model.add(Embedding(MAX_NB_WORDS, EMBEDDING_DIM, input_length=X.shape[1]))
@@ -59,7 +51,7 @@ model.add(LSTM(100, dropout=0.2, recurrent_dropout=0.2))
 model.add(Dense(13, activation='softmax'))
 model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
-epochs = 5
+epochs = 3
 batch_size = 64
 
 history = model.fit(X_train, Y_train, epochs=epochs, batch_size=batch_size,validation_split=0.1,callbacks=[EarlyStopping(monitor='val_loss', patience=3, min_delta=0.0001)])
